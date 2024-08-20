a373f93d464eccd3f37edf7f76249bca0810295f
Diego Bellani <bellanidiego@gmail.com>
2024-08-20 14:59:59 +0200

    added gui command

--- build.sh
+++ build.sh
@@ -10,5 +10,6 @@ nextpnr-ecp5 --85k \
 	--lpf ulx3s_v20.lpf \
 	--textcfg ulx3s_out.config \
 	--package CABGA381
+# nextpnr-ecp5 --json ${f}.json --lpf ulx3s_v20.lpf --package CABGA381 --gui
 ecppack ulx3s_out.config ulx3s.bit
 fujprog ulx3s.bit
