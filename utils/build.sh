1505beafe357210312f70619c6b26dc6a8dff578
Diego Bellani <bellanidiego@gmail.com>
2024-10-06 17:47:58 +0200

    Made it so that we can select the input of the viewer.

--- utils/build.sh
+++ utils/build.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-cc -framework Cocoa -framework AVFoundation main.m -o main
+cc -framework Cocoa -framework AVFoundation -g main.m -o main
 mkdir -p main.app/Contents/MacOS
 cp -f Info.plist main.app/Contents
-cp -f main/Contents/MacOS
+cp -rf main main.dSYM main.app/Contents/MacOS
--- utils/main.m
+++ utils/main.m
@@ -3,6 +3,7 @@
 
 @interface AppDelegate : NSObject <NSApplicationDelegate>
 @property (strong) NSWindow *window;
+@property (strong) AVCaptureDeviceDiscoverySession *discoverySession;
 // TODO: try to remove those references.
 @property (strong) AVCaptureSession *captureSession;
 @property (strong) AVCaptureVideoPreviewLayer *captureVideoPreviewLayer;
@@ -15,6 +16,7 @@
 @implementation AppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    NSError *error = nil;
     {
         NSRect frame = NSMakeRect(100, 100, 640, 480);
         NSUInteger styleMask = NSWindowStyleMaskTitled
@@ -25,10 +27,31 @@
                                                     backing:NSBackingStoreBuffered
                                                       defer:NO];
 
-        [self.window setTitle:@"My Programmatic Window"];
+        [self.window setTitle:@"Viewer"];
         [self.window makeKeyAndOrderFront:nil];
         [self.window center];
     }
+
+    NSMenu *mainMenu = [[NSMenu alloc] initWithTitle:@"MainMenu"];
+    [NSApp setMainMenu:mainMenu];
+
+    {
+        NSMenuItem *appMenuItem = [[NSMenuItem alloc] init];
+        [mainMenu addItem:appMenuItem];
+            NSMenu *appMenu = [[NSMenu alloc] initWithTitle:@"App"];
+            [appMenuItem setSubmenu:appMenu];
+
+            NSMenuItem *quitMenuItem = [[NSMenuItem alloc] initWithTitle:@"Quit"
+                                                                  action:@selector(quitApp:)
+                                                           keyEquivalent:@"q"];
+            [quitMenuItem setTarget:self];
+            [appMenu addItem:quitMenuItem];
+    }
+
+    NSMenuItem *devicesMenuItem = [[NSMenuItem alloc] init];
+    [mainMenu addItem:devicesMenuItem];
+    NSMenu *devicesMenu = [[NSMenu alloc] initWithTitle:@"Devices"];
+    [devicesMenuItem setSubmenu:devicesMenu];
 
     {
         self.videoView = [[NSView alloc] initWithFrame:self.window.contentView.bounds];
@@ -37,20 +60,26 @@
     }
 
     {
-        NSError *error = nil;
         self.captureSession = [AVCaptureSession new];
         self.captureSession.sessionPreset = AVCaptureSessionPreset640x480;
-        AVCaptureDeviceDiscoverySession *discoverySession = nil;
-        discoverySession = [AVCaptureDeviceDiscoverySession discoverySessionWithDeviceTypes:@[AVCaptureDeviceTypeExternal]
-                                                                                  mediaType:AVMediaTypeVideo
-                                                                                   position:AVCaptureDevicePositionUnspecified];
-        AVCaptureDevice *captureDevice = discoverySession.devices.firstObject;
-        AVCaptureDeviceInput *captureDeviceInput = [AVCaptureDeviceInput deviceInputWithDevice:captureDevice
-                                                                                         error:&error];
-        if ([self.captureSession canAddInput:captureDeviceInput]) {
-            [self.captureSession addInput:captureDeviceInput];
-        } else NSLog(@"can't add input");
+        self.discoverySession = [AVCaptureDeviceDiscoverySession discoverySessionWithDeviceTypes:@[AVCaptureDeviceTypeExternal]
+                                                                                       mediaType:AVMediaTypeVideo
+                                                                                        position:AVCaptureDevicePositionUnspecified];
+
+        NSInteger tag = 0;
+        for (AVCaptureDevice *device in self.discoverySession.devices) {
+			NSString tagString = tag <= 9 ? [@(tag) stringValue] : @"";
+            NSMenuItem *menuItem = [[NSMenuItem alloc] initWithTitle:device.localizedName
+                                                              action:@selector(deviceSelected:)
+                                                       keyEquivalent:tagString];
+            menuItem.tag = tag++;
+            [menuItem setTarget:self];
+            [devicesMenu addItem:menuItem];
+			// This is an abomination I know, but it works
+            if (tag-1 == 0) [self deviceSelected:menuItem];
+        }
     }
+
 
     {
         self.captureVideoPreviewLayer = [AVCaptureVideoPreviewLayer layerWithSession:self.captureSession];
@@ -62,7 +91,33 @@
     [self.captureSession startRunning];
 }
 
+- (void)deviceSelected:(id)sender {
+    NSError *error = nil;
+    [self.captureSession beginConfiguration];
+
+    for (AVCaptureInput *captureInput in self.captureSession.inputs) {
+        [self.captureSession removeInput:captureInput];
+    }
+
+    NSInteger tag = ((NSMenuItem *)sender).tag;
+    AVCaptureDevice *captureDevice = self.discoverySession.devices[tag];
+
+    AVCaptureDeviceInput *captureDeviceInput = [AVCaptureDeviceInput deviceInputWithDevice:captureDevice
+                                                                                     error:&error];
+    if ([self.captureSession canAddInput:captureDeviceInput]) {
+        [self.captureSession addInput:captureDeviceInput];
+    }
+
+    [self.captureSession commitConfiguration];
+}
+
+- (void)quitApp:(id)sender {
+    [NSApp terminate:self];
+}
+
 // FIXME: this is not called even with NSSupportsSuddenTermination in Info.plist
+// (now it seem to work but it is the selector called by the NSMenu that closes
+// the application.)
 - (NSApplicationTerminateReply)applicationShouldTerminate:(NSApplication *)app {
     return NSTerminateNow;
 }
